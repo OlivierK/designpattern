8824ff51e45e0c23898d04ca0bc1314215df1ad0
Mathieu Sgard <mathieu.sgard@free.fr>
2010-10-07 11:08:53 +0200

    Etape 0

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MAIN
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE designp_test
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include "designp.cpp"
+
+BOOST_AUTO_TEST_CASE(designp_test1){
+  
+}
